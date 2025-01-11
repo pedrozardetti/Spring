a013e9278d45b7aee2935ab454dd4b402070e23f
Pedro Zardetti <pedrozardetti1@gmail.com>
2025-01-10 21:20:42 -0300

    feat: add entity using lombok validation

--- gestao-vagas/src/main/java/br/com/gestao_vagas/modules/candidate/CandidateEntity.java
+++ gestao-vagas/src/main/java/br/com/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,2 +1,33 @@
-package br.com.gestao_vagas.modules.candidate;public class CandidateEntity {
+package br.com.gestao_vagas.modules.candidate;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CandidateEntity {
+
+    private UUID id;
+    @NotNull(message = "Usuário Inválido!")
+    private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo username não deve conter espaços!")
+    private String username;
+
+    @Email(message = "E-mail Inválido!")
+    private String email;
+
+    @Size(min = 3, max = 12, message = "Senha inválida!")
+    private String password;
+    private String description;
+    private String curriculum;
 }
