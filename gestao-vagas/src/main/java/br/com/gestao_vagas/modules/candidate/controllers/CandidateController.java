a1d11c3908d21c73914c04ef96f8ea67842e9f87
Pedro Zardetti <pedrozardetti1@gmail.com>
2025-01-10 21:21:40 -0300

    feat: add test

--- gestao-vagas/src/main/java/br/com/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ gestao-vagas/src/main/java/br/com/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,2 +1,20 @@
-package br.com.gestao_vagas.modules.candidate.controllers;public class CandidateController {
+package br.com.gestao_vagas.modules.candidate.controllers;
+
+import br.com.gestao_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @PostMapping("/")
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        System.out.println("Candidato");
+        System.out.println(candidateEntity.getEmail());
+    }
+
 }
