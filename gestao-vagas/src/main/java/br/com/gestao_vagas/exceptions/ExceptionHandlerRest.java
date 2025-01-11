0dd31e333b4d0b7554a3eb28ad96ba541d787e09
Pedro Zardetti <pedrozardetti1@gmail.com>
2025-01-10 22:02:35 -0300

    feat: create ExceptionHandler

--- gestao-vagas/src/main/java/br/com/gestao_vagas/exceptions/ExceptionHandlerRest.java
+++ gestao-vagas/src/main/java/br/com/gestao_vagas/exceptions/ExceptionHandlerRest.java
@@ -1,13 +1,41 @@
 package br.com.gestao_vagas.exceptions;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class ExceptionHandlerRest {
 
-    @
-    public void handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    private MessageSource messageSource;
 
+    public ExceptionHandlerRest(MessageSource message) {
+        this.messageSource = message;
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorMessageDTO>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<ErrorMessageDTO> dto = new ArrayList<>();
+        e.getBindingResult().getFieldErrors().forEach(err -> {
+            String message = messageSource.getMessage(err, LocaleContextHolder.getLocale());
+
+            ErrorMessageDTO error = new ErrorMessageDTO(message, err.getField());
+            dto.add(error);
+        });
+
+        return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserNotFound.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFound e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
 }
