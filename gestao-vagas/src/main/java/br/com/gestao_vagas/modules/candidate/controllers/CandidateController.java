ea89d239ec1d7238b4b19bd5dd90bf337d485f73
Pedro Zardetti <pedrozardetti1@gmail.com>
2025-01-16 18:29:30 -0300

    feat: add useCase to separate responsabilities

--- gestao-vagas/src/main/java/br/com/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ gestao-vagas/src/main/java/br/com/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -4,8 +4,10 @@ import br.com.gestao_vagas.exceptions.UserFoundException;
 import br.com.gestao_vagas.exceptions.UserNotFound;
 import br.com.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,17 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CandidateController {
 
     @Autowired
-    private CandidateRepository candidateRepository;
+    private CreateCandidateUseCase createCandidateUseCase;
 
     @PostMapping("/")
-    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail()).ifPresent((user) ->
-        {
-            throw new UserFoundException();
-        });
-        return this.candidateRepository.save(candidateEntity);
-
-
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        try {
+            var result = this.createCandidateUseCase.execute(candidateEntity);
+            return ResponseEntity.ok(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 }
