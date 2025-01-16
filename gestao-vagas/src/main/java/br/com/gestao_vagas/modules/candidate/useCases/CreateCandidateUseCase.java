6999e307d1e4e4fafc98222060e0ca604c794d60
Pedro Zardetti <pedrozardetti1@gmail.com>
2025-01-16 18:29:51 -0300

    feat: create useCase

--- gestao-vagas/src/main/java/br/com/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ gestao-vagas/src/main/java/br/com/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,2 +1,24 @@
-package br.com.gestao_vagas.modules.candidate.useCases;public class CreateCandidateUseCase {
+package br.com.gestao_vagas.modules.candidate.useCases;
+
+import br.com.gestao_vagas.exceptions.UserFoundException;
+import br.com.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.gestao_vagas.modules.candidate.CandidateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateCandidateUseCase {
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public CandidateEntity execute(CandidateEntity candidateEntity) {
+        this.candidateRepository
+                .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+        return this.candidateRepository.save(candidateEntity);
+    }
 }
+
+
