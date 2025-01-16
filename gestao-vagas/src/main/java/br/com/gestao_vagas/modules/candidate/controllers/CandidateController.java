a6e0e685b7b74e09a5fca9166971dba397424e6e
Pedro Zardetti <pedrozardetti1@gmail.com>
2025-01-16 17:45:41 -0300

    fix: adjust return

--- gestao-vagas/src/main/java/br/com/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ gestao-vagas/src/main/java/br/com/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -2,7 +2,9 @@ package br.com.gestao_vagas.modules.candidate.controllers;
 
 import br.com.gestao_vagas.exceptions.UserNotFound;
 import br.com.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CandidateRepository candidateRepository;
+
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity) throws UserNotFound {
-        throw new UserNotFound("Usuário não encontrado!");
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        return this.candidateRepository.save(candidateEntity);
 
     }
 
