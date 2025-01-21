ead74ffa5d1eaaad5c2eb6cdd66535f450a3a8bf
Pedro Zardetti <pedrozardetti1@gmail.com>
2025-01-21 11:26:44 -0300

    feat: add use case for job creation

--- gestao-vagas/src/main/java/br/com/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
+++ gestao-vagas/src/main/java/br/com/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
@@ -1,2 +1,18 @@
-package br.com.gestao_vagas.modules.company.useCases;public class CreateJobUseCase {
+package br.com.gestao_vagas.modules.company.useCases;
+
+import br.com.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.gestao_vagas.modules.company.repositories.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateJobUseCase {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    public JobEntity execute(JobEntity jobEntity) {
+        return this.jobRepository.save(jobEntity);
+
+    }
 }
