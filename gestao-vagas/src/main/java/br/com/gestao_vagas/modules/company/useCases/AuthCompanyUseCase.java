c12cc284f2f578d237591c6b5d1222448bb831bd
Pedro Zardetti <pedrozardetti1@gmail.com>
2025-01-24 17:15:57 -0300

    feat: add token jwt for decoding

--- gestao-vagas/src/main/java/br/com/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ gestao-vagas/src/main/java/br/com/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -2,7 +2,10 @@ package br.com.gestao_vagas.modules.company.useCases;
 
 import br.com.gestao_vagas.modules.company.dtos.AuthCompanyDTO;
 import br.com.gestao_vagas.modules.company.repositories.CompanyRepository;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -12,13 +15,16 @@ import javax.naming.AuthenticationException;
 @Service
 public class AuthCompanyUseCase {
 
+    @Value("${security.token.secret}")
+    private String secretKey;
+
     @Autowired
     private CompanyRepository companyRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public void execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
                 () -> {
                     throw new UsernameNotFoundException("Company not found");
@@ -29,6 +35,12 @@ public class AuthCompanyUseCase {
         if (!passwordMatches) {
             throw new AuthenticationException();
         }
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create().withIssuer("javagas")
+                .withSubject(company.getId().toString())
+                .sign(algorithm);
+        return token;
+
     }
 
 }
