905e0bbbdc6c4d64c75579373d6997dfa28acabd
Pedro Zardetti <pedrozardetti1@gmail.com>
2025-01-27 19:31:02 -0300

    fix: add transactional annotation

--- gestao-vagas/src/main/java/br/com/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ gestao-vagas/src/main/java/br/com/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,19 +4,29 @@ import br.com.gestao_vagas.exceptions.UserFoundException;
 import br.com.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.gestao_vagas.modules.candidate.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository
                 .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
+
         return this.candidateRepository.save(candidateEntity);
     }
 }
