865d4c712673d7e44c7a6a567a0c94e90ede3575
Pedro Zardetti <pedrozardetti1@gmail.com>
2025-01-27 21:34:32 -0300

    feat: create new exception

--- gestao-vagas/src/main/java/br/com/gestao_vagas/exceptions/AuthenticationException.java
+++ gestao-vagas/src/main/java/br/com/gestao_vagas/exceptions/AuthenticationException.java
@@ -1,2 +1,8 @@
-package br.com.gestao_vagas.exceptions;public class AuthenticationException {
+package br.com.gestao_vagas.exceptions;
+
+public class AuthenticationException extends Exception {
+
+    public AuthenticationException(String message) {
+        super(message);
+    }
 }
