89ae8ed701934655998155caeedfeeb16c5077c7
Pedro Zardetti <pedrozardetti1@gmail.com>
2025-01-27 21:35:52 -0300

    feat: create usecase for candidate

--- gestao-vagas/src/main/java/br/com/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
+++ gestao-vagas/src/main/java/br/com/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -1,2 +1,35 @@
-package br.com.gestao_vagas.modules.candidate.useCases;public class AuthCandidateUseCase {
+package br.com.gestao_vagas.modules.candidate.useCases;
+
+
+import br.com.gestao_vagas.exceptions.AuthenticationException;
+import br.com.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthCandidateUseCase {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void execute(AuthCandidateRequestDTO authCandidateRequestDTO) throws AuthenticationException {
+        var candidate = this.candidateRepository.findByUsername(authCandidateRequestDTO.username())
+                .orElseThrow(() -> {
+                            throw new UsernameNotFoundException("Username/password incorrect");
+                        });
+
+        var passwordMatches = this.passwordEncoder
+                .matches(authCandidateRequestDTO.password(), candidate.getPassword());
+
+        if (!passwordMatches) {
+            throw new AuthenticationException("Username/password incorrect");
+        }
+    }
+
 }
