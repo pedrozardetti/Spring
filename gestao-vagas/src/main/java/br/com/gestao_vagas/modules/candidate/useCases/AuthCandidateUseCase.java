9f88cca4c75c52a5425cd0ebe98b13087fd14135
Pedro Zardetti <pedrozardetti1@gmail.com>
2025-01-27 21:52:53 -0300

    feat: add token

--- gestao-vagas/src/main/java/br/com/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
+++ gestao-vagas/src/main/java/br/com/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -4,13 +4,24 @@ package br.com.gestao_vagas.modules.candidate.useCases;
 import br.com.gestao_vagas.exceptions.AuthenticationException;
 import br.com.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import br.com.gestao_vagas.modules.candidate.dto.AuthCandidateResponseDTO;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
 @Service
 public class AuthCandidateUseCase {
+
+    @Value("${security.token.secret.candidate}")
+    private String secretKey;
 
     @Autowired
     private CandidateRepository candidateRepository;
@@ -18,7 +29,7 @@ public class AuthCandidateUseCase {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public void execute(AuthCandidateRequestDTO authCandidateRequestDTO) throws AuthenticationException {
+    public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO) throws AuthenticationException {
         var candidate = this.candidateRepository.findByUsername(authCandidateRequestDTO.username())
                 .orElseThrow(() -> {
                             throw new UsernameNotFoundException("Username/password incorrect");
@@ -30,6 +41,19 @@ public class AuthCandidateUseCase {
         if (!passwordMatches) {
             throw new AuthenticationException("Username/password incorrect");
         }
-    }
 
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create()
+                .withIssuer("javagas")
+                .withSubject(candidate.getId().toString())
+                .withClaim("roles", Arrays.asList("candidate"))
+                .withExpiresAt(Instant.now().plus(Duration.ofMinutes(10)))
+                .sign(algorithm);
+
+        var authCandidateResponse = AuthCandidateResponseDTO.builder()
+                .access_token(token)
+                .build();
+
+        return authCandidateResponse;
+    }
 }
