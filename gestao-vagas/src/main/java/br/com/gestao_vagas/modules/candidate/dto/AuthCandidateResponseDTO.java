fcc826b0da278912a492d93e6a030723850ff7f7
Pedro Zardetti <pedrozardetti1@gmail.com>
2025-01-29 20:41:36 -0300

    feat: add DTO for candidate auth

--- gestao-vagas/src/main/java/br/com/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
+++ gestao-vagas/src/main/java/br/com/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -12,4 +14,5 @@ import lombok.NoArgsConstructor;
 public class AuthCandidateResponseDTO {
 
     private String access_token;
+    private Long expires_in;
 }
